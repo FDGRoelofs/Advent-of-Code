d40785a8ea9b86e9e301ca883a1249aa9b2268e0
Floris Roelofs <floris_roelofs@hotmail.com>
2023-12-17 20:21:33 +0100

    Begonnen aan dag 6

---
+++ Advent of Code/2023Solutions/2023Dag06.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code
+{
+    public class Dag2306 : Dag
+    {
+        public Dag2306(string s) : base(s)
+        {
+
+        }
+        public string[] debugstring = new string[10];
+        public override void Puzzel1()
+        {
+            
+            
+        }
+        new Dictionary<int,bool> results = new Dictionary<int, bool>();
+        public int countWins(int recordTime, int recordDistance)
+        {
+            int middle = recordTime / 2;
+            int pivot = middle;
+            int lowestwin = -1;
+            int hbl;
+            int highestwin = -1;
+            int ltl;
+            if(winRace(pivot, recordDistance, recordTime))
+            {
+                lowestwin = pivot;
+                highestwin = pivot;
+            }
+            else
+            {
+                hbl = pivot;
+                ltl = pivot;
+            }
+            bool bottomfound = false;
+            int hbl;
+            while(!bottomfound)
+            {
+                if(lowestwin > 0)
+                    pivot = pivot / 2;
+                else
+                    pivot = 
+                    
+            }
+                
+            return 0;
+        }
+
+        public int calculateSpeed(int holdTime)
+        {
+            int speed = 0;
+            int growth = 1;
+            while(holdTime > 0)
+            {
+                speed += growth;
+                growth++;
+                holdTime--;
+            }
+            return speed;
+        }
+
+        public bool winRace(int holdTime, int goalDistance, int goalTime)
+        {
+            int runningTime = goalTime - holdTime;
+            int distance = runningTime * calculateSpeed(holdTime);
+            results.Add(runningTime, distance > goalDistance);
+            return (distance > goalDistance);
+        }
+
+        public override void Puzzel2()
+        {
+            
+        }
+    }
+}
